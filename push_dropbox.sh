78f0e572e38cf22ba7006049f3a1bc4df207ce4a
Jason Kuruzovich <jkuruzovich@gmail.com>
2019-08-23 12:24:47 -0400

    push to public

--- push_dropbox.sh
+++
@@ -1,2 +0,0 @@
-curl https://www.dropbox.com/s/9y1qhlcfpln7zgy/book.xlsx > book.xlsx
-source push.sh
---
+++ push_shared.sh
@@ -0,0 +1,2 @@
+cp /Users/jasonkuruzovich/Dropbox/0_Classes/analytics/2019_fall/IntroMLShared/book.xlsx .
+source push.sh
